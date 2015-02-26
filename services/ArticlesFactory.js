a987a3c222db60fcc638e2f272e736c37a0e00dc
Debbie Cravey and Arsi <argoncode18@gmail.com>
2015-02-26 10:42:46 -0800

    add heart graphic for adding votes to an (h)article

--- services/ArticlesFactory.js
+++ services/ArticlesFactory.js
@@ -2,10 +2,19 @@ hackedNews.factory('ArticlesFactory', function ArticlesFactory() {
   var factory = {};
   factory.articles = [];
   factory.addArticle = function() {
-    factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1 });
+    factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1, counter: 0 });
     factory.articleName = null;
     factory.articleLink = null;
     console.log(factory.articles);
   };
+
+  factory.articleAddVote = function(article) {
+    article.counter += 1;
+    console.log(article);
+    console.log(factory.articles);
+  };
+
+
+
   return factory;
 });
