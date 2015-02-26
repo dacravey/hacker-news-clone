c337807761b9a45b7902b811a3b1f91aa00148b2
Debbie Cravey and Arsi <cinemaestro@yahoo.com>
2015-02-26 11:07:37 -0800

    add sorting functionality to votes

--- services/ArticlesFactory.js
+++ services/ArticlesFactory.js
@@ -1,15 +1,16 @@
 hackedNews.factory('ArticlesFactory', function ArticlesFactory() {
   var factory = {};
   factory.articles = [];
+
   factory.addArticle = function() {
-    factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1, counter: 0 });
+    factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1, vote: 0 });
     factory.articleName = null;
     factory.articleLink = null;
     console.log(factory.articles);
   };
 
   factory.articleAddVote = function(article) {
-    article.counter += 1;
+    article.vote += 1;
     console.log(article);
     console.log(factory.articles);
   };
