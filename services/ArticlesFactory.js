4ca4e65ee23c0d60fa3cdf92009e1e64f31dbbd0
Debbie Cravey and Arsi <cinemaestro@yahoo.com>
2015-02-26 09:40:12 -0800

    add link validation

--- services/ArticlesFactory.js
+++ services/ArticlesFactory.js
@@ -4,6 +4,7 @@ hackedNews.factory('ArticlesFactory', function ArticlesFactory() {
   factory.addArticle = function() {
     factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1 });
     factory.articleName = null;
+    factory.articleLink = null;
     console.log(factory.articles);
   };
   return factory;
